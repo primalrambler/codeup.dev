f34ff7fab7492bfb14a377541599e3237549db24
primalrambler <dcarroll.movil@gmail.com>
2016-07-26 11:17:15 -0500

    initial commit

---
+++ public/p001_resume.php
@@ -0,0 +1,227 @@
+<!DOCTYPE>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	<meta lang="en">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <title>RESUME|CARROLL</title>
+
+</head>
+
+<body>
+<!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="/p001_index.php">DjCARROLL</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                    	<a href="/#">RESUME</a>
+                    </li>
+                    <li>
+                        <a href="/p001_portfolio.php">PORTFOLIO</a>
+                    </li>
+                    <li>
+                        <a href="/p001_contact">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+
+    <div class="content-section-a">
+        <div class="container">
+        	<a name="web-development"></a>
+            <div class="row">
+                <div class="col-lg-7 col-sm-6">
+                    <div class="clearfix"></div>
+
+                    <h2 class="section-heading">WEB DEVELOPMENT</h2>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+                </div>
+
+                <div class="col-lg-4 col-lg-offset-1 col-sm-6">
+                	
+                    <h3>LANGUAGES</h3>
+                    <p>replace with icons or images of all languages. make it two columns.</p>
+                    <ul>
+                    	<li>HTML</li>
+                    	<li>CSS</li>
+                    	<li>JavaScript</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-a -->
+
+	
+	<div class="content-section-b">
+        <div class="container">
+
+        	<a name="work-experience"></a>
+            <div class="row">
+                <div class="col-lg-7 col-sm-6">
+                	<h2 class="section-heading">WORK EXPERIENCE</h2>
+                	<hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                  
+                    <h4 >PROJECT ENGINEER • PROJECT MANAGER</h4>
+                 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                 	<h4>PROJECT ENGINEER • DESIGN TASK LEAD</h4>
+                 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                 	<h4>TRAFFIC ENGINEER</h4>
+                 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                 	<h4>SPACE OPERATIONS OFFICER</h4>
+                 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-1 col-sm-6">
+                	
+                    <h3>WORK HISTORY</h3>
+                    	<h4><a href="http:hntb.com">HNTB </a><small>2008 - 2016</small></h4>
+                    	<p>Project Engineer • Squad Leader</p>
+                    	<p>Project Manager • Design Task Lead</p>
+                    	<p> San Antonio, Texas </p>
+                    	<br>
+                    	<h4>CAMACHO-HERNANDEZ <small>2007 - 2008</small></h4>
+                    	<p>Project Engineer</p>
+                    	<p> San Antonio, Texas </p>
+                    	<br>
+                    	<h4>JACOBS ENGINEERING <small>2005 - 2007</small></h4>
+                    	<p>Project Engineer</p>
+                    	<p> San Antonio, Texas </p>
+                    	<br>
+                    	<h4>LOCKWOOD, ANDREWS & NEWNAM <small>2003 - 2005</small></h4>
+                    	<p>Engineer</p>
+                    	<p> San Antonio, Texas </p>
+                    	<br>
+                    	<h4>PAPE-DAWSON ENGINEERS <small>2001 - 2003</small></h4>
+                    	<p>Traffic Engineer</p>
+                    	<p> San Antonio, Texas </p>
+                    	<br>
+                    	<h4>2<sup>nd</sup> SPACE LAUNCH SQUADRON <small>1995 - 1997</small></h4>
+                    	<p>Chief, Operations Training • Launch Crew Commander • Satellite Operations Commander</p>
+                    	<p> Vandenberg AFB, California </p>
+                    	<br>
+                    	<h4>8<sup>th</sup> MISSILE WARNING SQUADRON <small>1993 - 1995</small></h4>
+                    	<p>Missile Warning Crew Commander</p>
+                    	<p> Eldorado AS, Texas </p>
+
+
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-a -->   
+
+    <div class="content-section-a">
+        <div class="container">
+        	<a name="education"></a>
+
+			<div class="row">
+                <div class="col-md-12">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">EDUCATION • CERTIFICATIONS</h2>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-4 col-sm-6">
+                    <h3>CODEUP</h3>
+                    <p>July - November 2016</p>
+                    <p class="lead">Full Stack Web Development</p>
+                </div>
+
+                <div class="col-lg-4 col-sm-6">
+                    <h3>TEXAS A&M UNIVERSITY</h3>
+                    <p>January - December 2000</p>
+                    <p class="lead">Master of Engineering</p>
+                    <p>Civil Engineering, GPA 3.7X
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <h3>TEXAS A&M UNIVERSITY</h3>
+                    <p>September 1997 - December 1999</p>
+                    <p class="lead">Bachelor of Science</p>
+                    <p>Civil Engineering, GPA 3.62
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <h3>US AIR FORCE ACADEMY</h3>
+                    <p>June 1989 - June 1993</p>
+                    <p class="lead">Bachelor of Science</p>
+                    <p>General Studies, Dean's List
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <h3>PROFESSIONAL ENGINEER</h3>
+                    <p>Active</p>
+                    <p class="lead">Texas Board of Professional Engineers</p>
+                    <p>License No. 95615
+                </div>
+
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-a -->
+
+
+
+</html>
+</body>
