2b097b711aaf01980d37d0cb1652c1d7bdf7f2ae
primalrambler <primal.rambler@gmail.com>
2016-07-28 17:39:08 -0500

    Added Bootstrap CSS classes for the form groups and the input groups.

--- public/login_form.php
+++ public/login_form.php
@@ -5,25 +5,63 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Login Form</title>
-	</head>
-	<body>
-		<form method="GET" action="/login_form.php">
-			<p>
-				<label for="username">Username or email address </label>
-				<input type="text" name="username" placeholder="username or email">
-			</p>
-			<p>
-				<label for="password">Password</label>
-				<input type="password" name="password" placeholder="password">
-			</p>
-			<input type="submit" value="Login">
-			<p>
-				<label><input type="checkbox" name="remember[]" value="1" checked>Remember me </label>
-			</p>
+<head>
+    <meta charset="utf-8">
 
-		</form>
-	<body>
+        <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Website Font style -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+
+    <title>Login Form</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <form class="form-horizontal" method="post" action="#">
+            <div class="form-group">
+                <label for="username" class="cols-sm-2 control-label">Username</label>
+                <div class="cols-sm-10">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                        <input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="password" class="cols-sm-2 control-label">Password</label>
+                <div class="cols-sm-10">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                        <input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group ">
+                <button type="button" class="btn btn-primary btn-lg btn-block login-button">Sign in</button>
+            </div>
+            <div class="login-register">
+                <a href="/adlister.register.php">Create account</a> or <a href="reset_password.php">reset password</a>
+             </div>
+        </form>
+    </div>
+
+
+
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+    </script>
+<body>
 </html>
