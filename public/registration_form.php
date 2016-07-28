4883083f97a872c7d149e8ae89a9efdc6774dcf9
primalrambler <primal.rambler@gmail.com>
2016-07-28 17:55:30 -0500

    add bootstrap css to form for form groups and input groups

--- public/registration_form.php
+++ public/registration_form.php
@@ -5,41 +5,98 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Registration Form</title>
-	</head>
-	<body>
-		<form>
-			<p>
-				<label for="firstName">First Name  </label>
-				<input id="firstName" name="firstName" type="text" placeholder="John">
-			</p>
-			<p>
-				<label for="lastName">Last Name  </label>
-				<input id="lastName" name="lastName" type="text" placeholder="Doe">
-			</p>
-				<label for="_email">Email Address  </label>
-				<input id="_email" name="_email" type="email" placeholder="jd1993@mail.com">
-			<p>
-			</p>
-				<label for="username">Username  </label>
-				<input id="username" name="username" type="text" placeholder="awesome_sauce">
-			<p>
-				<label for="password">Password  </label>
-				<input id="password" name="password" type="password" placeholder="$ecret$auce">
-			</p>
-			<p>
-				<label for="pConfirmation">Confirm your Password  </label>
-				<input id="pConfirmation" name="pConfirmation" type="password" placeholder="retype it">
-			</p>
-			<p>
-				<label>Sign me up for the newsletter </label>
-				<input id="newsletter" name="newsletter" value="yes" type="checkbox" checked>
-			</p>
-			<p>
-				<button type="submit" name="register"value="yes">Submit</button>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		</form>
-	</body>
+    <!-- Website CSS style -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Website Font style -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	
+	<title>Registration Form</title>
+
+</head>
+
+<body>
+	<!-- Form  -->
+    <div class="container">
+            <form class="form-horizontal" method="post" action="#">
+                <div class="form-group">
+                    <label for="first-name" class="cols-sm-2 control-label">Your First Name</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                            <input type="text" class="form-control" name="first-name" id="first-name"  placeholder="Enter your First Name"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="last-name" class="cols-sm-2 control-label">Your Last Name</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                            <input type="text" class="form-control" name="last-name" id="last-name"  placeholder="Enter your Last Name"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="cols-sm-2 control-label">Your Email</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                            <input type="email" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="username" class="cols-sm-2 control-label">Username</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+                            <input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password" class="cols-sm-2 control-label">Password</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                            <input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                            <input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group ">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block login-button">Register</button>
+                </div>
+                <div class="login-register">
+                    <a href="/adlister.login.php">Login</a>
+                 </div>
+            </form>
+            <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		    <!-- Latest compiled and minified JavaScript -->
+		    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+		        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+		    </script>
+    </div>
+    <!-- End Form -->
+
+
+
+</body>
 </html>
