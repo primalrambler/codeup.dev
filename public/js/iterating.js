4604b4be2ebdb3c9fe9a87dde8ab12b2e09c3938
primalrambler <primal.rambler@gmail.com>
2016-08-02 15:25:33 -0500

    added for and forEach to iterate through an array

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -6,7 +6,7 @@
     var names = ["Skipper","Gilligan","Ginger","Thurston","Mary Ann","The Professor","Eunice"];
 
     // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
+    console.log("The number of elements in the array is " + names.length);
 
     // TODO: Create log statements that will print each of the names array elements individually.
 
@@ -14,6 +14,30 @@
     console.log(names[1]);
     console.log(names[2]);
     console.log(names[3]);
+	
+
+
+    //reverse array - not using anything special i.e. not using .reverse() function
+
+    var reverseNames = [];
+
+    for (var i = 0; i < names.length; i +=1) {
+    	reverseNames[i] = names[(names.length -1)  - i ];
+    }
+    console.log(names);
+    console.log(reverseNames);
+
+    console.log("Using a for loop")
+    for (var i = 0; i < names.length; i +=1){
+    	console.log(names[i]);
+    }
+
+    console.log("Using a forEach function")
+    names.forEach(function (element,index,array){
+    	// console.log(element == array[index]);
+    	// console.log(element == names[index]);
+    	console.log(element);
+    });
 
 
 })();
