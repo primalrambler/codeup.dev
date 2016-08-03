83094b1284ea95d7bdffad0af72f3aa92686b7af
primalrambler <primal.rambler@gmail.com>
2016-08-03 11:24:15 -0500

    sum the digits using string and array commands

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -1,18 +1,22 @@
 "use strict";
-// (function() {
-
-var myArray = ["Gilligan", "Skipper", "Mary Ann", "Ginger", "The Professor"]
 
 
-function randomIndex (array) {
-	return Math.floor(Math.random() * array.length);
+function sumTheDigits(number) {
+
+	// sum the digits of a number
+	//turn number into a string
+	var numberString = number.toString();
+
+	//turn that string into an array of digits
+	var numberArray = numberString.split("");
+
+	//loop through that array and add together the digits
+	
+	var sum = 0;
+
+	numberArray.forEach(function(number){
+		sum += parseInt(number);
+	})
+
+	return sum;
 }
-
-function sample (array) {
-	var result = array[randomIndex(array)];
-	return result;
-}
-
-console.log(sample(myArray));
-
-// })();
