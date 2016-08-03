0c6e7f9deb0e95ed8e367e1d7ddb5bcd00e9581a
primalrambler <primal.rambler@gmail.com>
2016-08-03 14:46:56 -0500

    object lesson - includes object within object example

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -1,22 +1,20 @@
 "use strict";
 
-
-function sumTheDigits(number) {
-
-	// sum the digits of a number
-	//turn number into a string
-	var numberString = number.toString();
-
-	//turn that string into an array of digits
-	var numberArray = numberString.split("");
-
-	//loop through that array and add together the digits
-	
-	var sum = 0;
-
-	numberArray.forEach(function(number){
-		sum += parseInt(number);
-	})
-
-	return sum;
+var player1 = {
+	"firstName" : "Bob",
+	"lastName" : "Bobberson"
 }
+
+var whiteDie = {
+	"rollValue" : null,
+	"owner" : player1,
+	"numberofSides" : 12,
+	"color" : "white",
+	"rollDie" : function() {
+		this.rollValue = Math.ceil(Math.random() * this.numberofSides)
+		return this.rollValue;
+	}
+};
+
+//to get the first name of the owner
+whiteDie.owner.firstName;
