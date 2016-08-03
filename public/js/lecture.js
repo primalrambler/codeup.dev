c9bd49e5210228dac5205c7eaa95a48517169bd2
primalrambler <primal.rambler@gmail.com>
2016-08-03 15:40:21 -0500

    JSON syntax example

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -1,20 +1,31 @@
 "use strict";
 
-var player1 = {
-	"firstName" : "Bob",
-	"lastName" : "Bobberson"
+var car = {
+	"make" : ,
+	"model" : {
+		"name" : "sonata",
+		"suspension": "sport",
+		"trim" : null
+
+	},
+	"year" : 1998,
+	"miles" : 20000,
+	"mpg" : 19,
+	"color" : "blue",
+	//internally defined object
+	"currentOwner" : {
+		"firstName" : "Dan",
+		"lastName" : "Carroll"
+	},
+	//array of objects
+	"previousOwners": [
+		{
+			"firstName" : "Bob",
+			"lastName" : "Bobberson"
+		},
+		{
+			"firstName" : "Dave",
+			"lastName" : "Davidson"
+		}
+	]
 }
-
-var whiteDie = {
-	"rollValue" : null,
-	"owner" : player1,
-	"numberofSides" : 12,
-	"color" : "white",
-	"rollDie" : function() {
-		this.rollValue = Math.ceil(Math.random() * this.numberofSides)
-		return this.rollValue;
-	}
-};
-
-//to get the first name of the owner
-whiteDie.owner.firstName;
