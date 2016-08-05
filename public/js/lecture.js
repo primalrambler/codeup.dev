f7151a5a9e95dbc459434feda39651db05a68758
primalrambler <primal.rambler@gmail.com>
2016-08-05 16:20:46 -0500

    lecture examples for event listeners and dom changes

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -1,8 +1,4 @@
 "use strict";
 
-
-var delay = 5000; // delay time in milliseconds
-
-var timeoutId = setTimeout(function () {
-    alert('Here is a delayed hello!');
-}, delay);
+var navbarElement = document.getElementById('navbar');
+console.log(navbarElement);
