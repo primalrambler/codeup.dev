f38b065225c0f58bd69c9d289f5de9fe8d1aa9f5
primalrambler <primal.rambler@gmail.com>
2016-08-08 16:42:17 -0500

    lecture example

--- public/js/lecture.js
+++ public/js/lecture.js
@@ -1,4 +1,25 @@
 "use strict";
 
-var navbarElement = document.getElementById('navbar');
-console.log(navbarElement);
+
+function addTopping () {
+	var newTopping = '';
+
+	newTopping += "<li>";
+	newTopping += this.innerText;
+	newTopping += "</li>";
+
+	selectedToppings.innerHTMl += newTopping;
+
+	this.innerText += ' selected!';
+	this.style.backgroundColor = 'yellow';
+}
+
+
+var toppings = document.getElementsByClassName('topping');
+var selectedToppings = document.getElementById('selectedToppings');
+var topping;
+
+for (var i = 0; i < toppings.length; i +=1 ) {
+	topping = toppings[i];
+	topping.addEventListener('click',addTopping)
+}
