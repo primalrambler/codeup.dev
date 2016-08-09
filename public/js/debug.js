392d78625d2ee711a2247e363a87f8771d814c9d
primalrambler <primal.rambler@gmail.com>
2016-08-09 09:44:17 -0500

    debug exercise with event listeners and buttons

--- public/js/debug.js
+++ public/js/debug.js
@@ -1,75 +1,82 @@
 'use strict';
 
-function sayHello (name) {
-    return 'hello ' + name;
+// we're expecting to see an alert when we click the button
+
+document.getElementById('my-btn1').addEventListener("click",function(e){
+    alert('you clicked the button!');
+});
+
+// we're expecting to see the font color of all the paragraphs turn blue
+
+var paragraphs = document.getElementsByClassName('lorem');
+
+for (var i = 0; i < paragraphs.length; i++) {
+    var p = paragraphs[i];
+    p.style.color = 'blue';
 }
 
-sayHello('zach');
 
-// expect to see 'hello zach' in the console
+// expect to see an alert
 
-// ----------------------------------------------------------------------------
+var myButton = document.getElementById('my-btn');
 
-function sayHelloToEveryoneExceptZach (name) {
-    if (name == 'zach') {
-        return 'goodbye';
-    } else {
-        return 'hello ' + name;
-    }
+myButton.addEventListener('click', alertMe);
+
+function alertMe () {
+    alert('hey! you clicked a thing');
 }
 
-console.log(sayHelloToEveryoneExceptZach('Ryan'));
+// the 1 button doesn't work
+// the 2 button replaces whatever is in the display
+// the 5 button breaks everything
+// the clear button doesn't do anything
 
-// expect to see 'hello Ryan' in the console
+// food for thought: it looks like an awful lot of duplication in our code for
+// selecting and adding an event listener to all the buttons
 
-// ----------------------------------------------------------------------------
-
-// returns a random number between 1 and 100
-function getRandomNumber () {
-    return Math.floor(Math.random() * 100) + 1;
+function clearClicked (e) {
+    display.value = '';
 }
 
-// returns true if number is odd, otherwise false
-function isOdd (number) {
-    return number % 2 == 1;
+var display = document.getElementById('display');
+var clear   = document.getElementById('clear');
+
+// var btn1 = document.getElementById('btn-1');
+// var btn2 = document.getElementById('btn-2');
+// var btn3 = document.getElementById('btn-3');
+// var btn4 = document.getElementById('btn-4');
+// var btn5 = document.getElementById('btn-5');
+
+
+var displayButtons = document.getElementsByClassName('buttons')
+console.log(displayButtons);
+
+function displayText (e) {
+    display.value += this.innerText;
 }
 
-var rand = getRandomNumber();
-
-if (isOdd(rand)) {
-    console.log(rand + ' is odd');
-} else {
-    console.log(rand + ' is not odd');
+for (var i=0; i < displayButtons.length; i += 1){
+    displayButtons[i].addEventListener('click',displayText);
 }
 
-// expect to see '4 is not odd'
+// btn1.addEventListener('click', function(e){
+//     display.innerText = 1;
+// });
 
-// ----------------------------------------------------------------------------
+// btn2.addEventListener('click', function(e){
+//     display.value = 2;
+// });
 
-for (var i = 1; i <= 10; i += 1) {
-    console.log(i);
-}
+// btn3.addEventListener('click', function(e){
+//     display.value += 3;
+// });
 
-// expect to see 1 through 10
+// btn4.addEventListener('click', function(e){
+//     display.value += 4;
+// });
 
-// ----------------------------------------------------------------------------
+// btn5.addEventListener('click', function(e){
+//     display += 5;
+// });
 
-for (var i = 10; i > 0; i -= 1) {
-    console.log(i);
-}
-
-// expect to see 10 through 1
-
-// ----------------------------------------------------------------------------
-
-function sum(x, y) {
-    return x + y;
-}
-
-var x = 3;
-var y = 5;
-
-console.log(sum(x,y));
-
-// expect to see 8
-
+clear.addEventListener('click', sclearClicked);
