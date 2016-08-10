a0bf8eb274d23216076529a58b4563d0fd239e17
primalrambler <primal.rambler@gmail.com>
2016-08-10 15:17:19 -0500

    added marker animation with a drop and also added a bounce on the click of info window with a timeout function to stop the bouncing.

--- public/js/google_maps_api.js
+++ public/js/google_maps_api.js
@@ -1,29 +1,61 @@
 // (function() {
-        "use strict";
+    "use strict";
 
-        // Set our map options
-        var mapOptions = {
-            // Set the zoom level
-            zoom: 13,
+var mapOptions = {
+    // Set the zoom level
+    zoom: 18,
 
-            // This sets the center of the map at our location
-            center: {
-                lat:  29.426791,
-                lng: -98.489602
-            }
-        };
+    // This sets the center of the map at our location
+    center: {
+        lat:  29.568503,
+        lng: -98.487987
+    }
+};
+
+var restaurant = {lat:  29.568503, lng: -98.487987};
+
+// Render the map
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: restaurant,
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP
+    });
+
+    marker.addListener('click', toggleBounce);
 
 
-        // Render the map
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
 
-        //add a marker for a favorite restaurant
-        var restaurant = { lat: 29.568503, lng: -98.487987};
 
-        var  favRestaurant = new google.maps.Marker({
-        	
-        	position: restaurant,
-        	
-        	map: map
+  // info window
+
+    var contentString = '<h2>Taste of Asia</h2>'+
+    '<p><h3>Favorite Dishes</h3></p>'+
+    '<ul>'+
+    '<li>Chicken Lemongrass</li>'+
+    '<li>Spring Rolls</li>'+
+    '</ul>';
+
+
+        var infowindow = new google.maps.InfoWindow({
+         content: contentString
         });
+   
+        marker.addListener('click', function(){
+            infowindow.open(map,marker);
+            setTimeout (function(){
+                marker.setAnimation(null);
+            },1000);
+        });
+
+
     // })();
