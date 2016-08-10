3a3fd8cea405677c265b021a37b1e5fc8ec43795
primalrambler <primal.rambler@gmail.com>
2016-08-10 13:40:04 -0500

    added a map marker

--- public/js/google_maps_api.js
+++ public/js/google_maps_api.js
@@ -4,7 +4,7 @@
         // Set our map options
         var mapOptions = {
             // Set the zoom level
-            zoom: 19,
+            zoom: 13,
 
             // This sets the center of the map at our location
             center: {
@@ -13,6 +13,17 @@
             }
         };
 
+
         // Render the map
         var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        //add a marker for a favorite restaurant
+        var restaurant = { lat: 29.568503, lng: -98.487987};
+
+        var  favRestaurant = new google.maps.Marker({
+        	
+        	position: restaurant,
+        	
+        	map: map
+        });
     // })();
