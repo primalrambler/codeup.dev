52a6cc268edee1dac2f01c6d9100e657137b6980
primalrambler <primal.rambler@gmail.com>
2016-08-16 15:00:08 -0500

    created lat/long input boxes. weather and map data refreshes on search button submittal

--- public/js/weather-map.js
+++ public/js/weather-map.js
@@ -5,6 +5,22 @@ $(document).ready(function() {
 //openweathermap.org
 const myAPIKey = 'b112d425689409f17145cc7ecad0f6bd';
 
+var lat = 29.426791;
+var lng = -98.489602;
+
+// Render the map
+var mapOptions = {
+        // Set the zoom level
+        zoom: 15,
+        // This sets the center of the map at our location
+        center: {
+            lat:  lat,
+            lng: lng
+        }
+    };
+
+var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
 
 //display the weather information into daily forecast box
 
@@ -14,7 +30,7 @@ function weatherDisplay (data){
 	$("#insertLocale").append(cityName);
 
     	forecasts.forEach(function(forecast){
-        	console.log (forecast);
+        	// console.log (forecast);
         	var windSpeed = Math.round(forecast.speed * 2.23694); //converts mps to mph
             var contents = '';
             contents += '<div class="weather-box">';
@@ -31,24 +47,65 @@ function weatherDisplay (data){
         });
     }
 
-//get weather data 
-$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-	APPID: 	myAPIKey,
-	q: 		"San Antonio, TX",
-	cnt: 	"3", 
-	units: 	"imperial"
-}).done(function(data){
-	console.log(data);
-	weatherDisplay(data);
-}).fail(function(xhr, err, msg){
-	alert ('something went wrong');
-	// console.log(xhr);
-	// console.log(err);
-	// console.log(msg);
-});
+//get weather data
+function getWeather (){
+	$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+		APPID: 	myAPIKey,
+		// q: 		"San Antonio, TX", //search by city
+		lat:    lat,	
+		lon: 	lng,
+		cnt: 	"3", 
+		units: 	"imperial"
+	}).done(function(data){
+		console.log(data);
+		weatherDisplay(data);
+	}).fail(function(xhr, err, msg){
+		alert ('something went wrong');
+		// console.log(xhr);
+		// console.log(err);
+		// console.log(msg);
+	});
+}
 
 
+function getLatLong (){
+    lat = parseFloat($('#latitude').val());
+    lng = parseFloat($('#longitude').val());
+    mapOptions = {
+        // Set the zoom level
+        zoom: 15,
+        // This sets the center of the map at our location
+        center: {
+            lat:  lat,
+            lng: lng
+        }
+    };
+}
 
+function clearData (){
+	$("#insertForecast").fadeOut();
+	$("#insertForecast").html('');
+	$("#insertForecast").fadeIn();
+	$("#insertLocale").fadeOut();
+	$("#insertLocale").html('');
+	$("#insertLocale").fadeIn();
+
+}
+
+function redisplayMap (){
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	console.log(mapOptions);
+}
+
+    $('#searchButton').click(function(){
+    	getLatLong();
+    	clearData();
+    	getWeather();
+    	redisplayMap();
+    });
+
+//load San Antonio weather and map on page load event 
+getWeather();
 
 
 });  //close wrapper
