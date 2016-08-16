265c2511a3c718724c54fd6c6e208d469cc23545
primalrambler <primal.rambler@gmail.com>
2016-08-16 12:02:15 -0500

    basic 3-day forecast

--- public/js/weather-map.js
+++ public/js/weather-map.js
@@ -8,31 +8,32 @@ const myAPIKey = 'b112d425689409f17145cc7ecad0f6bd';
 
 //display the weather information into daily forecast box
 
-function weatherDisplay (forecast){
-        // forecasts.forEach(function(forecast){
-        	var windSpeed = Math.round(forecast.wind.speed * 2.23694); //converts mps to mph
-        	
+function weatherDisplay (data){
+	var forecasts = data.list;
+	console.log(forecasts);
+    	forecasts.forEach(function(forecast){
+        	console.log (forecast);
+        	var windSpeed = Math.round(forecast.speed * 2.23694); //converts mps to mph
             var contents = '';
             contents += '<div class="weather-box">';
-            contents += '<p class="temperature">' + Math.round(forecast.main.temp_max) + 
-            			"&#176 / " + Math.round(forecast.main.temp_min) + '&#176</p>';
+            contents += '<p class="temperature">' + Math.round(forecast.temp.max) + 
+            			"&#176 / " + Math.round(forecast.temp.min) + '&#176</p>';
             contents += '<img src="http://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png">'
 			contents += '<p class="data"><span class="data-label">' + forecast.weather[0].main 
 						+ ': </span><span class="details">' + forecast.weather[0].description + '</span>';
-			contents += '<p class="data"><span class="data-label">Humidity: </span><span class="details">' + forecast.main.humidity + '%</span>';
+			contents += '<p class="data"><span class="data-label">Humidity: </span><span class="details">' + forecast.humidity + '%</span>';
 			contents += '<p class="data"><span class="data-label">Wind: </span><span class="details">' + windSpeed + ' mph</span>';
-			contents += '<p class="data"><span class="data-label">Pressure: </span><span class="details">' + forecast.main.pressure + '</span> hPa';
-
-
+			contents += '<p class="data"><span class="data-label">Pressure: </span><span class="details">' + forecast.pressure + '</span> hPa';
             contents += '</div>';
             $("#insertForecast").append(contents);
-        // });
+        });
     }
 
 //get weather data 
-$.get("http://api.openweathermap.org/data/2.5/weather", {
+$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
 	APPID: 	myAPIKey,
 	q: 		"San Antonio, TX",
+	cnt: 	"3", 
 	units: 	"imperial"
 }).done(function(data){
 	console.log(data);
