27a3a15c98506668cc1535c9f5e2912f10d4af2c
primalrambler <primal.rambler@gmail.com>
2016-08-17 16:08:35 -0500

    added info window with current conditions where you drop the pin

--- public/js/weather-map.js
+++ public/js/weather-map.js
@@ -15,42 +15,35 @@ var myLatLng = {lat: 29.426791, lng:-98.489602};  //initial position centered on
 var lat = myLatLng.lat; //
 var lng = myLatLng.lng;
 var map;
-
 var mapOptions = {
 	zoom: 8,
 	center: myLatLng
 	};
+var contentString = '';
 
 map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 	var marker = new google.maps.Marker({
 	position: myLatLng,
 	map: map,
-	draggable: true
+	draggable: true,
+	title: 'Click for current conditions',
+	animation: google.maps.Animation.DROP
 	});
 
 	
 	var infowindow = new google.maps.InfoWindow({
-	content: '<p>Marker Location:' + marker.getPosition() + '</p>'
+	content: ''
 	});
 
-	google.maps.event.addListener(marker, 'click', function() {
+google.maps.event.addListener(marker, 'click', function() {
 	infowindow.open(map, marker);
 	});
 
-	google.maps.event.addListener(marker, 'dragend', function () {
-		myLatLng = marker.getPosition();
-		var tempLat = marker.getPosition().lat;
-		var tempLng = marker.getPosition().lng;
-		console.log(marker.getPosition());
-		console.log(tempLat);
-		console.log(tempLng);
-	});
-	// }
 
 	function dateGrabber (data){
 		var unixDate = new Date(data.dt * 1000);
-		console.log(unixDate);
+		// console.log(unixDate);
 		var month = unixDate.getMonth();
 		var date = unixDate.getDate();
 		var weekday = unixDate.getDay();
@@ -87,12 +80,13 @@ function weatherDisplay (data){  //display the weather information into daily fo
 
 	var forecasts = data.list;
 	var cityName = '<h2>' + data.city.name + '</h2>'
+	$("#insertLocale").empty();
 	$("#insertLocale").append(cityName);
+    $("#insertForecast").empty();
 
     	forecasts.forEach(function(forecast){
 			var forecastDate = dateGrabber(forecast);
-        	console.log (forecast);
-        	var windSpeed = Math.round(forecast.speed * 2.23694); //converts mps to mph
+        	// console.log (forecast);
             var contents = '';
             contents += '<div class="weather-box">';
             contents += '<p class="date">' + forecastDate + '</p>';
@@ -102,14 +96,45 @@ function weatherDisplay (data){  //display the weather information into daily fo
 			contents += '<p class="data"><span class="data-label">' + forecast.weather[0].main 
 						+ ': </span><span class="details">' + forecast.weather[0].description + '</span>';
 			contents += '<p class="data"><span class="data-label">Humidity: </span><span class="details">' + forecast.humidity + '%</span>';
-			contents += '<p class="data"><span class="data-label">Wind: </span><span class="details">' + windSpeed + ' mph</span>';
+			contents += '<p class="data"><span class="data-label">Wind: </span><span class="details">' + Math.round(forecast.speed) + ' mph</span>';
 			contents += '<p class="data"><span class="data-label">Pressure: </span><span class="details">' + forecast.pressure + '</span> hPa';
             contents += '</div>';
             $("#insertForecast").append(contents);
         });
 }
 
+function infoWindowContent (data) {
+		console.log(data);
+    var contents = '';
+        contents += '<div>';
+        contents += '<p class="info-window-data data-label">Current conditions</p>'
+        contents += '<p class="info-window-temp">' + Math.round(data.main.temp) + '&#176</p>';
+        contents += '<img class="info-icons" src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">'
+		contents += '<p class="info-window-data"><span class="data-label">' + data.weather[0].main 
+					+ ': </span><span class="details">' + data.weather[0].description + '</span>';
+		contents += '<p class="info-window-data"><span class="data-label">Humidity: </span><span class="details">' + data.main.humidity + '%</span>';
+		contents += '<p class="info-window-data"><span class="data-label">Wind: </span><span class="details">' + Math.round(data.wind.speed) + ' mph</span>';
+		contents += '<p class="info-window-data"><span class="data-label">Pressure: </span><span class="details">' + data.main.pressure + '</span> hPa';
+        contents += '</div>';
+        infowindow.setContent(contents);
+        
+}
 
+
+
+function getCurrentConditions(){
+
+	$.get("http://api.openweathermap.org/data/2.5/weather", {
+		APPID: 	myAPIKey,
+		lat:    lat,	
+		lon: 	lng,
+		units: "imperial"
+	}).done(function(data){
+		infoWindowContent(data);
+	}).fail(function(xhr, err, msg){
+		alert ('something went wrong');
+	});
+}
 
 
 //get weather data
@@ -126,27 +151,17 @@ function getMultiDayForecasts (){
 		weatherDisplay(data);
 	}).fail(function(xhr, err, msg){
 		alert ('something went wrong');
-		// console.log(xhr);
-		// console.log(err);
-		// console.log(msg);
 	});
 }
 
-function clearData (){
-	$("#insertForecast").fadeOut();
-	$("#insertForecast").html('');
-	$("#insertForecast").fadeIn();
-	$("#insertLocale").fadeOut();
-	$("#insertLocale").html('');
-	$("#insertLocale").fadeIn();
-}
+
 
 //add listener for new map/weather location based on marker drag
 google.maps.event.addListener(marker, 'dragend', function (event) {
     lat = event.latLng.lat();
     lng = event.latLng.lng();
 	getMultiDayForecasts();
-	clearData();
+	contentString = getCurrentConditions();
 	map.setCenter(marker.position);
 	marker.setMap(map);
 });  //close listener
