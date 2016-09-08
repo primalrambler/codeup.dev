06558b49a65eb8b199b1fd1590072ecaba54172f
primalrambler <primal.rambler@gmail.com>
2016-09-08 15:34:12 -0500

    html set up for game play. two buttons and a message area.

---
+++ public/ping.php
@@ -0,0 +1,51 @@
+<?php
+$counter = 0;
+$message = "Ping da Pong"
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title>PING</title>
+	<!-- Bootstrap core CSS -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" 
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<style type="text/css">
+	body {
+		padding-top: 70px;
+	}
+	.button {
+		padding-top: 10px;
+		padding-bottom: 30px;
+		margin: auto;
+	}
+	</style>
+
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-3 button text-left">
+				<h1>PING</h1>
+				<a href="/pong.php?value=<?= $counter +1 ?>?type=hit" class="btn btn-primary btn-lg">HIT 
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
+				<p></p>
+				<a href="/pong.php?value=0?type=miss" class="btn btn-primary btn-lg">MISS 
+					<span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
+			</div>
+			<div class="col-xs-6">
+				<div class="jumbotron text-center">
+					<h1><?= $message ?></h1>
+					<?php ($counter == 0)? "" : '<h2>Number of Consecutive Hits</h2>'; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
