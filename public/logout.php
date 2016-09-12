48df14898d0961b02ea04747c189ab758a6a8320
primalrambler <primal.rambler@gmail.com>
2016-09-12 15:47:29 -0500

    added die commands after all redirects.

--- public/logout.php
+++ public/logout.php
@@ -2,13 +2,21 @@
 
 session_start();
 
+
 function clearSession()
 {
     // clear $_SESSION array
     session_unset();
 
-    // delete session data on the server and send the client a new cookie
-    session_regenerate_id(true);
+    // delete session data on the server
+    session_destroy();
+
+    // ensure client is sent a new session cookie
+    session_regenerate_id();
+
+    // start a new session - session_destroy() ended our previous session so
+    // if we want to store any new data in $_SESSION we must start a new one
+    session_start();
 }
 
 
