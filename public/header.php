5f13feccf305017957b503158d1809b3d68fa768
primalrambler <primal.rambler@gmail.com>
2016-09-13 10:57:25 -0500

    cleaned up templating and php includes

---
+++ public/header.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap core CSS -->
+    <link 
+        rel="stylesheet" 
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+        crossorigin="anonymous"
+    >
+    <title>Template</title>
+    <!--[if lt IE 9]>
+    <script src="http://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js">
+    </script>
+    <script src="http://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js">
+    </script>
+    <![endif]-->
+</head>
---
+++ public/index_template.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<?php include 'header.php' ?>
+<body>
+	<?php include 'navbar.html' ?>
+	<h1>Hello There</h1>
+
+<?php include 'footer.php' ?>
+</body>
+</html>
