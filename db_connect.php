990585641784b6dc0cfe13c6d5d4a35b0e4511a7
primalrambler <primal.rambler@gmail.com>
2016-09-22 15:50:23 -0500

    created generic db connection file.

---
+++ db_connect.php
@@ -0,0 +1,10 @@
+<?php
+
+// Get new instance of PDO object
+// $dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'password');
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
