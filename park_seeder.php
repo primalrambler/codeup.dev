18900ba6d15320819b5e3a9c6a6e5c620e30b1b5
primalrambler <primal.rambler@gmail.com>
2016-09-22 16:51:21 -0500

    for each loop to insert data from external parks information array

--- park_seeder.php
+++ park_seeder.php
@@ -1,8 +1,14 @@
 <?php
-
+//connect to database
 require_once ('db_connect_park.php');
 require_once ('db_connect.php');
 
+//include external parks data
+require_once ('parks_array.php');
 
-
-for each
+//insert data from parks array into database
+foreach ($parks as $park) {
+    $query = "INSERT INTO national_parks (name,location,date_established,area_in_acres) VALUES ('{$park['name']}','{$park['location']}','{$park['date_established']}','{$park['area_in_acres']}')";
+	$dbc->exec($query);
+	echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
+}
