d218cc14ac449094c37487c110c0c5d7bbe67fcf
primalrambler <primal.rambler@gmail.com>
2016-09-23 12:24:08 -0500

    initial upload. functions for populating table and pagination

---
+++ public/js/national_parks.js
@@ -0,0 +1,20 @@
+// (function() {
+'use strict';
+
+//function to complete the list for the pagination
+
+
+//function to populate the table based on the page
+
+
+//event listeners to get the right page number
+
+
+
+//logic to control back and forward on page controllers
+//disable back/forward if at the extremes
+
+
+
+
+// })();
