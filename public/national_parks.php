939e713b369d715067335ce5389fdb5c33918c90
primalrambler <primal.rambler@gmail.com>
2016-09-23 12:16:32 -0500

    added table html, added bootstrap pagination

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once('db_connect_parks');
 
 
 
@@ -9,13 +9,50 @@
 
 <!DOCTYPE html>
 <html>
-<?php include_once('header.php'); ?>
+<head>
+	<?php include_once('header.php'); ?>
+	<title>Parks</title>
+<head>
 <body>
-<div class= "container">
+	<div class="page-header">
+	  <h1 style='padding-left: 70px;'>NATIONAL PARKS <small>our common treasure</small></h1>
+	</div>
+	<div class= "container">
+
+		<table id="parks" class="table table-striped">
+			<thead>
+				<tr>
+					<th>Park Name</th>
+					<th>Location</th>
+					<th>Year Established</th>
+					<th>Size (ac)</th>
+			</thead>
+			<tbody id="insertParks"></tbody>
+		</table>
 
 
-	
-</div>
+		<nav aria-label="Page navigation">
+		  <ul class="pagination">
+		    <li>
+		      <a href="#" aria-label="Previous">
+		        <span aria-hidden="true">&laquo;</span>
+		      </a>
+		    </li>
+		    <li><a href="#">1</a></li>
+		    <li><a href="#">2</a></li>
+		    <li><a href="#">3</a></li>
+		    <li><a href="#">4</a></li>
+		    <li><a href="#">5</a></li>
+		    <li>
+		      <a href="#" aria-label="Next">
+		        <span aria-hidden="true">&raquo;</span>
+		      </a>
+		    </li>
+		  </ul>
+		</nav>
+
+
+	</div>
 
 <?php include_once('footer.php'); ?>
 <body>
