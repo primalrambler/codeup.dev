a8290ab234e10dae4b736b1b8bfac2494fa4b8ec
primalrambler <primal.rambler@gmail.com>
2016-09-23 11:43:14 -0500

    initial upload, basic html structure

---
+++ public/national_parks.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<?php include_once('header.php'); ?>
+<body>
+<div class= "container">
+
+
+	
+</div>
+
+<?php include_once('footer.php'); ?>
+<body>
+</html>
