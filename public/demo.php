8f4cadecccc9d4f8faafc2e040fe3e52ab3ea8e2
primalrambler <primal.rambler@gmail.com>
2016-09-23 11:40:44 -0500

    in class example of fetching data from sql using php

--- public/demo.php
+++ public/demo.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once ('db_connect_parks');
+
+$stmt = $dbc->query("SELECT * FROM national_parks;");
+
+
+function getparks($stmt)
+{
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	foreach ($rows as $row){
+		echo $row['name'];
+		echo "<br>";
+	}
+}
+
+getParks($stmt);
