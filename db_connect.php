535a61816cc4b3dd7c9ed024d38eb45376034712
primalrambler <primal.rambler@gmail.com>
2016-09-23 15:57:36 -0500

    added __DIR__ syntax

--- db_connect.php
+++ db_connect.php
@@ -7,4 +7,6 @@ $dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+//for checking if there's a connection
+// echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
