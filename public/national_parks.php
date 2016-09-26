b85149e42b700a53187712605e02274244f82a2b
primalrambler <primal.rambler@gmail.com>
2016-09-26 11:16:00 -0500

    fixed problem with page and offset

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,20 +1,18 @@
 <?php
 require_once (__DIR__.'/../db_connect_park.php');
 require_once (__DIR__.'/../Input.php');
+
 $limit = 4;
-$offset = (intval(Input::get('page')) -1) * $limit;
+
+
 function getPageCount ($dbc,$limit) {
 	$stmt = $dbc->query('SELECT * FROM national_parks');
 	$number_of_parks = $stmt->rowCount();
 	$number_of_pages = ceil($number_of_parks/$limit);
 	return $number_of_pages;
 }
-$number_of_pages = getPageCount($dbc,$limit);
-// $offset = 5;
-$stmt = $dbc->query('SELECT * FROM national_parks LIMIT '.$limit.' OFFSET '.$offset.'');
-$parks = $stmt->fetchAll(PDO::FETCH_NUM);
-    
-function getTableRowData ($parks) {
+
+function createTable ($parks) {
 	$tableRowHtml = '';
 	foreach ($parks as $park) {
 		$tableRowHtml .='<tr>';  //start the row
@@ -26,14 +24,26 @@ function getTableRowData ($parks) {
 	return $tableRowHtml;
 }
 
-
-//page controller needs to return...
-//table html
-//number of pages to generate
-
-
-$insertParks = getTableRowData($parks);
 $number_of_pages = getPageCount($dbc,$limit);
+
+//set page number and pass to view for page control
+$page = (Input::has('page')) ? intval(Input::get('page')) : 1;
+if ($page < 1) {
+	$page = 1;
+} else if ($page > $number_of_pages) {
+	$page = $number_of_pages;
+}
+
+
+
+
+$offset = ($limit * $page) - $limit;
+$stmt = $dbc->query('SELECT * FROM national_parks LIMIT '.$limit.' OFFSET '.$offset.'');
+$parks = $stmt->fetchAll(PDO::FETCH_NUM);
+$insertParks = createTable($parks);
+    
+
+
 
 ?>
 
@@ -64,7 +74,7 @@ $number_of_pages = getPageCount($dbc,$limit);
 		<nav aria-label="Page navigation">
 		  <ul class="pagination">
 		    <li>
-		      <a href="#" aria-label="Previous">
+		      <a href="national_parks.php?page=<?= $page-1; ?>" aria-label="Previous">
 		        <span aria-hidden="true">&laquo;</span>
 		      </a>
 		    </li>
@@ -72,7 +82,7 @@ $number_of_pages = getPageCount($dbc,$limit);
 		    	<li><a href="national_parks.php?page=<?= $i; ?>"><?= $i; ?></a></li>
 			<?php endfor; ?>
 		    <li>
-		      <a href="#" aria-label="Next">
+		      <a href="national_parks.php?page=<?= $page+1; ?>" aria-label="Next">
 		        <span aria-hidden="true">&raquo;</span>
 		      </a>
 		    </li>
