ce3e044b53495061ee2f69ca620c750d3b3b7200
primalrambler <primal.rambler@gmail.com>
2016-09-27 15:51:49 -0500

    overwrote constructor for square

--- Square.php
+++ Square.php
@@ -2,6 +2,14 @@
 
 class Square extends Rectangle
 {
+
+	public function __construct($height)
+	{
+		$this->height = $height;
+		$this->width = $height;
+	}
+
+
 	public function perimeter()
 	{
 		return 4 * $this->height;
--- public/shapes_test.php
+++ public/shapes_test.php
@@ -5,18 +5,15 @@ require_once(__DIR__.'/../square.php');
 
 
 
-$shape_01 = new Square(4,4);
-echo 'The area is '.$shape_01->area().PHP_EOL;
-echo 'The perimeter is '.$shape_01->perimeter().PHP_EOL;
+$rectangle = new rectangle(4,7);
+echo 'The area is '.$rectangle->area().PHP_EOL;
 
 
-$shape_02 = new Square(5,5);
-echo 'The area is '.$shape_02->area().PHP_EOL;
-echo 'The perimeter is '.$shape_02->perimeter().PHP_EOL;
+$square = new Square(5);
+echo 'The area is '.$square->area().PHP_EOL;
+echo 'The perimeter is '.$square->perimeter().PHP_EOL;
 
 
-$shape_03 = new Square(6,6);
-echo 'The area is '.$shape_03->area().PHP_EOL;
-echo 'The perimeter is '.$shape_03->perimeter().PHP_EOL;
+
 
 
