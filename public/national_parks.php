a013a6faf78e344c4498b1c54d7426ee10857687
primalrambler <primal.rambler@gmail.com>
2016-09-27 09:51:25 -0500

    created page controller function and extract call. All PHP in a function now.

--- public/national_parks.php
+++ public/national_parks.php
@@ -2,13 +2,13 @@
 require_once (__DIR__.'/../db_connect_park.php');
 require_once (__DIR__.'/../Input.php');
 
-$limit = 4;
+define("LIMIT", 4);
 
 
-function getPageCount ($dbc,$limit) {
+function getPageCount ($dbc) {
 	$stmt = $dbc->query('SELECT * FROM national_parks');
 	$number_of_parks = $stmt->rowCount();
-	$number_of_pages = ceil($number_of_parks/$limit);
+	$number_of_pages = ceil($number_of_parks/LIMIT);
 	return $number_of_pages;
 }
 
@@ -24,20 +24,33 @@ function createTable ($parks) {
 	return $tableRowHtml;
 }
 
-$number_of_pages = getPageCount($dbc,$limit);
+function pageController ($dbc){
 
-//set page number and pass to view for page control
-$page = (Input::has('page')) ? intval(Input::get('page')) : 1;
-if ($page < 1) {
-	$page = 1;
-} else if ($page > $number_of_pages) {
-	$page = $number_of_pages;
+	$data = [];
+	$number_of_pages = getPageCount($dbc);
+
+	//set page number and pass to view for page control
+	$page = (Input::has('page')) ? intval(Input::get('page')) : 1;
+	if ($page < 1) {
+		$page = 1;
+	} else if ($page > $number_of_pages) {
+		$page = $number_of_pages;
+	}
+
+	$offset = (LIMIT * $page) - LIMIT;
+	$stmt = $dbc->query('SELECT * FROM national_parks LIMIT '.LIMIT.' OFFSET '.$offset.'');
+	$parks = $stmt->fetchAll(PDO::FETCH_NUM);
+	$parksTable = createTable($parks);
+
+	$data['number_of_pages'] = $number_of_pages;
+	$data['page'] = $page;
+	$data['parksTable'] = $parksTable;
+
+	return $data;
+
 }
 
-$offset = ($limit * $page) - $limit;
-$stmt = $dbc->query('SELECT * FROM national_parks LIMIT '.$limit.' OFFSET '.$offset.'');
-$parks = $stmt->fetchAll(PDO::FETCH_NUM);
-$insertParks = createTable($parks);
+extract(pageController($dbc));
     
 ?>
 
@@ -62,9 +75,8 @@ $insertParks = createTable($parks);
 					<th>Size (ac)</th>
 					<th>Description</th>
 			</thead>
-			<tbody id="insertParks"><?= $insertParks ?></tbody>
+			<tbody id="parksTable"><?= $parksTable ?></tbody>
 		</table>
-
 
 		<nav aria-label="Page navigation">
 		  <ul class="pagination">
@@ -84,6 +96,37 @@ $insertParks = createTable($parks);
 		  </ul>
 		</nav>
 	</div>
+
+
+	<div class="page-header">
+	  <h1 style='padding-left: 70px;'><small>Add a Park</small></h1>
+	</div>
+	<div class="container">
+		<form method="POST" action="/national_parks.php">
+			<div class="form-group">
+				<label for="name">Park Name</label>
+				<input type="text" class="form-control" id="name" name="name" placeholder="Park Name">
+			</div>
+			<div class="form-group">
+		    	<label for="location">State | US Territory</label>
+			    <input type="text" class="form-control" id="location" name="location" placeholder="State or US Territory the park it is located in...">
+			</div>
+		  	<div class="form-group">
+			    <label for="date_established">Date Established</label>
+			    <input type="text" class="form-control" id="date_established" name="date_established"placeholder="Date Established (YYYY-MM-DD)">
+		    </div>
+		    <div class="form-group">
+			    <label for="size_in_acres">Size</label>
+			    <input type="text" class="form-control" id="size_in_acres" name="size_in_acres"placeholder="Size in acres">
+		    </div>
+		    <div class="form-group">
+			    <label for="description">Description</label>
+			    <textarea class="form-control" id="description" name="description" placeholder="Brief description of the park"></textarea>
+		    </div>
+		    <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</div>
+
 
 <?php include_once('footer.php'); ?>
 <body>
