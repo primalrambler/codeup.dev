dbea6da9390f27574eb70cd887bb4152501f16f6
primalrambler <primal.rambler@gmail.com>
2016-09-28 15:25:05 -0500

    added private properties and included get and set functions to handle the new visibility

--- Log.php
+++ Log.php
@@ -2,23 +2,83 @@
 
 class Log
 {
-	public $filename;
-	public $datestamp;
-	public $timestamp;
-	public $handle;
-	// public $logDirectory = __DIR__ .'/logs/';
+	private $filename;
+	private $datestamp;
+	private $timestamp;
+	private $handle;
+	private $logDirectory;
 
-	public function __construct ($prefix = 'log')
+
+	public function __construct($prefix = 'log')
+	{
+		$this->logDirectory = __DIR__ . '/public/logs/';
+		$this->setDateStamp();
+		$this->setTimeStamp();
+		$this->setFilename($prefix);
+		$this->setHandle();
+	}
+
+
+	// public function __construct ($prefix = 'log')
+	// {
+	// 	$this->datestamp = date('Y-m-d');
+	// 	$this->timestamp = date('H:i:s');
+	// 	$this->filename = "$prefix-{$this->datestamp}.log";
+	// 	$this->handle = fopen($this->$logDirectory.$this->filename, 'a');
+	// }
+
+//----------- Setters
+
+	protected function setDateStamp ()
 	{
 		$this->datestamp = date('Y-m-d');
-		$this->timestamp = date('H:i:s');
-		$this->filename = "$prefix-{$this->datestamp}.log";
-		$this->handle = fopen($this->$logDirectory.$this->filename, 'a');
 	}
+
+	protected function setTimeStamp ()
+	{
+		$this->timestamp = date('H:i:s');
+	}
+
+	protected function setFilename($prefix)
+	{
+		$this->filename = "$prefix-".$this->getDateStamp().".log";
+	}
+
+	protected function setHandle()
+	{
+		$this->handle = fopen($this->getLogDirectory().$this->getFilename(), 'a');
+	}
+
+//----------- Getters
+
+
+	public function getDateStamp()
+	{
+		return $this->datestamp;
+	}
+	public function getTimeStamp()
+	{
+		return $this->timestamp;
+	}
+	public function getLogDirectory()
+	{
+		return $this->logDirectory;
+	}
+	public function getFilename()
+	{
+		return $this->filename;
+	}
+	public function getHandle()
+	{
+		return $this->handle;
+	}
+
+
+//----------- Operational
 
 	public function logMessage($level, $message)
 	{
-		fwrite($this->handle, PHP_EOL ."{$this->datestamp} {$this->timestamp} $level $message");
+		fwrite($this->getHandle(), PHP_EOL .$this->getDateStamp()." ".$this->getTimeStamp()." $level $message");
 	}
 
 	public function logError($message)
@@ -33,7 +93,7 @@ class Log
 
 	public function __destruct ()
 	{
-		fclose($this->handle);
+		fclose($this->getHandle());
 	}
 
 }
