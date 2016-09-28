6da808d165d4ddca513c3c0eb17dfade5f6af2b6
primalrambler <primal.rambler@gmail.com>
2016-09-28 15:29:58 -0500

    more class examples

--- public/demo.php
+++ public/demo.php
@@ -3,25 +3,26 @@
 class SomethingCool
 {
 	protected $attributes = [
-		'email'=> 'joe@example.com'
+		'email'=> 'joe@example.com',
 
 	];
 
 	public function __get($name)
 	{
-		if (isset($this->attributes['name'])){
-			return $this->attributes['name'];
+		if (isset($this->attributes[$name])){
+			return $this->attributes[$name];
 		}
 		return null;
 	}
 
 	public function __set($name,$value)
 	{
-		var_dump($name, $value);
+		$this->attributes[$name] = $value;
 	}
-
-
-
 }
 
 var_dump($cool_object->email);
+
+$cool_object->phone_number = '867-5309';
+
+var_dump($cool_object);
