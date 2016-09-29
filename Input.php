5d63a3b82e474007db538ab7008f996019ab3e30
primalrambler <primal.rambler@gmail.com>
2016-09-29 15:24:43 -0500

    updated to include throwing errors

--- Input.php
+++ Input.php
@@ -25,6 +25,41 @@ class Input
         return (self::has($key)) ? htmlspecialchars(strip_tags($_REQUEST[$key])) : $default;
     }
 
+    /**
+     * Check if a value is numeric
+     *
+     * @param string $key index to look for in index
+     * @return bool value passed in request
+     */
+    public static function checkNumeric($key)
+    {
+        return is_numeric(trim(static::get($key)));
+    }
+
+    public static function getString($key)
+    {
+        if (! self::has($key){
+            throw new Exception("$key does not exist")
+        } elseif (is_string(self::get($key)) && !self::checkNumeric($key)){
+            return self::get($key);
+        } else {
+            throw new Exception($key . 'value must be a string!');
+        }
+    }
+
+    public static function getNumber($key)
+    {
+        if (! self::has($key) && ! self::checkNumeric($key)){
+            throw new Exception($key . 'value must be a number!');
+        }
+        return floatval(self::get($key));
+    }
+
+
+
+
+
+
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
     // The Input class should not ever be instantiated, so we prevent the    //
