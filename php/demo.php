7e1687f588735c94e40d93280f5ae2590f3e5f7a
primalrambler <primal.rambler@gmail.com>
2016-10-03 16:45:55 -0500

    testing quiz code

--- php/demo.php
+++ php/demo.php
@@ -1,9 +1,16 @@
 <?php
-
-$e = [];
-
-try {
-	throw new Exception ('$key needs to be a string');
-} catch (Exception $e){
-	$e[] = $e->getMessage();
+class ParentClass {
+    public function sayHello() {
+        return 'Hello from the parent class!';
+    }
 }
+
+class Child extends ParentClass {
+    public function sayHello () {
+        return parent::sayHello() . ' And the child class!';
+    }
+}
+
+$child = new Child();
+
+var_dump($child->sayHello());
